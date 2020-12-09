83e71209340887bac201f25f24826d30c0d1f119
jccari <jhoelccari.23@gmail.com>
2020-12-08 23:10:24 -0500

    db

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,5 +11,6 @@ export const LARIAT = {
     }
 }
 
-export const DB_SOURCE = process.env.NODE_ENV== "development"? "public/Parlamentaries.db":"https://visaft.vercel.app/Parlamentaries.db"
+// export const DB_SOURCE = process.env.NODE_ENV== "development"? "public/Parlamentaries.db":"https://visaft.vercel.app/Parlamentaries.db"
+export const DB_SOURCE = process.env.NODE_ENV== "development"? "public/Parlamentaries.db" : "Parlamentaries.db"
 // export const DB_SOURCE = "/home/jccari/code/visaft/pages/api/Parlamentaries.db"
